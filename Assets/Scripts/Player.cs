d92742b6288377db6cbf0f68401373f926e0605f
36_TehKaiXuan <91871965+36-TehKaiXuan@users.noreply.github.com>
2021-10-20 13:27:34 +0800

    Made Unity Project Plus Imported Packages
    Also added restrictions to the player movements so player cant move outside of the playing field

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //Moving Up
+        if (verticalInput > 0)
+        {
+            if (transform.position.y < 3.9)
+            {
+                transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+            }
+        }
+        //Moving Down
+        if (verticalInput < 0)
+        {
+            if (transform.position.y > -3.9)
+            {
+                transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
